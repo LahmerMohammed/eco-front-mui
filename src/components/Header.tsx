bbc3faeca7c96e7a4b1db5f1b11fde92909cfddb
LahmerMohammed <tonysycho14@gmail.com>
2021-08-26 07:38:47 +0100

    create

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+// prettier-ignore
+import { makeStyles } from '@material-ui/core';
+import * as React from 'react';
+
+interface Props {
+
+}
+
+export function Header(props: Props) {
+
+const {  } = props;
+
+return (
+  <div >
+    
+  </div>
+  );
+}
+
