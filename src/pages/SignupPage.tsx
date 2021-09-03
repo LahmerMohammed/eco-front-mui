df4c03abadfea60188dcc0197bbbfefb36e3d94f
LahmerMohammed <tonysycho14@gmail.com>
2021-09-03 18:24:47 +0100

    create

---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,22 @@
+import { Theme, Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/styles';
+import * as React from 'react';
+
+interface Props {
+
+}
+
+export function SignupPage(props: Props) {
+
+const {  } = props;
+
+  return (
+    <div>
+      <Typography>Signup</Typography>
+    </div>
+);
+}
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+
+}));
