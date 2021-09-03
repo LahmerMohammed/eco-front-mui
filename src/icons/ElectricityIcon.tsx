40289ea9c20d7d6ff684030335e8d51be6195356
LahmerMohammed <tonysycho14@gmail.com>
2021-09-03 18:20:53 +0100

    create

---
+++ src/icons/ElectricityIcon.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon';
+
+
+
+export function ElectricityIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M19.0765 9.48063H12.1242L15.5905 0L5 14.5194H11.9522L8.48592 24L19.0765 9.48063Z" />
+    </SvgIcon>
+  );
+}
