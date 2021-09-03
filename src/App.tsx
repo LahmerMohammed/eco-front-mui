e8498eff2d33e9b420e158e5cf40de6850eef046
LahmerMohammed <tonysycho14@gmail.com>
2021-09-03 18:19:30 +0100

    add wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 // prettier-ignore
 import { Theme } from "@material-ui/core/styles";
-import { makeStyles } from "@material-ui/styles";
+import { createStyles, makeStyles } from "@material-ui/styles";
 import * as React from "react";
 import { Router } from "react-router-dom";
 import { RouterSwitch } from 'react-typesafe-routes';
 import { HomePage } from "./pages/HomePage";
 import { router } from "./Router";
 
-
-
 function App() {
 	const classes = useStyles();
 
 	return (
-			<HomePage/>
+			<div className={classes.root}>
+				<HomePage/>
+			</div>
 	);
 }
 
 
-const useStyles = makeStyles(() => {
-	root: {}
-})
+const useStyles = makeStyles((theme: Theme) => createStyles({
+	root: {
+		display: 'flex',
+		justifyContent: 'center',
+		maxWidth: '1990px'
+	}
+}));
 
 export default App;
