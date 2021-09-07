2626d0177a170ead7856a19b72a84490d612c661
LahmerMohammed <tonysycho14@gmail.com>
2021-09-07 12:13:10 +0100

    use 'section'

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -7,6 +7,7 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import * as React from 'react';
 import { Input } from './form/Input';
+import { Section } from './user/Section';
 
 
 interface UserProfileProps {
@@ -14,7 +15,7 @@ interface UserProfileProps {
   lastname: string,
   email: string,
   phonenumber: string,
-  birthdate: Date,
+  birthdate?: Date,
 }
 
 export function UserProfile(props: UserProfileProps) {
@@ -25,17 +26,21 @@ export function UserProfile(props: UserProfileProps) {
     ...props
   });
 
+  const [disabled, setDisabled] = React.useState(true);
 
 
   const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const target = evt.target;
-
+    const { name, value } = evt.target;
 
     setForm({
       ...form,
-      [target.name]: target.value,
+      [name]: value,
     });
 
+  }
+
+  const handleEditButton = () => {
+    setDisabled(false);
   }
 
   const handleDateChange = (date: Date | null) => {
@@ -50,91 +55,92 @@ export function UserProfile(props: UserProfileProps) {
   }
 
   return (
-    <Grid container style={style.root} >
-      <Grid item container alignItems='center' >
-        <AccountCircleRoundedIcon fontSize="large" style={style.headerIcon} />
-        <Typography variant="h4" style={style.title}>
-          Edit Profile
-        </Typography>
-      </Grid>
-      <Grid container item style={style.paperWrapper} rowGap="2rem">
-        <Paper style={style.paper}>
-          {/** add avatar */}
-          <Grid container wrap="wrap" style={style.form} rowGap="3rem">
-            <Grid item xs={12} md={6}>
-              <TextField
-                fullWidth
-                name="firstname"
-                label="Firstname"
-                value={form.firstname}
-                onChange={handleInputChange}
-                style={style.input}
-                required
-                sx={{ mr: { md: '1.5rem' } }}
-              />
+    <Section
 
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField
-                fullWidth
-                name="lastname"
-                label="Lastname"
-                value={form.lastname}
-                onChange={handleInputChange}
-                style={style.input}
-                required
-                sx={{ ml: { md: '1.5rem' } }}
+      icon={<AccountCircleRoundedIcon fontSize="large" style={style.headerIcon} />}
+      title="My Profile"
+      button="Edit Profile"
+      style={style.paperWrapper}
+      onClick={handleEditButton}
+    >
+      <Paper style={style.paper}>
+        <Grid container wrap="wrap" style={style.form} rowGap="3rem">
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              name="firstname"
+              label="Firstname"
+              value={form.firstname}
+              onChange={handleInputChange}
+              style={style.input}
+              required
+              sx={{ mr: { md: '1.5rem' } }}
+              disabled={disabled}
+            />
 
-              />
-
-            </Grid>
-            <Grid item xs={12} md={6}>
-
-              <TextField
-                fullWidth
-                name="email"
-                label="Email"
-                value={form.email}
-                onChange={handleInputChange}
-                style={style.input}
-                required
-                sx={{ mr: { md: '1.5rem' } }}
-
-              />
-
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField
-                fullWidth
-                name="phonenumber"
-                label="Phone Number"
-                value={form.phonenumber}
-                onChange={handleInputChange}
-                style={style.input}
-                sx={{ ml: { md: '1.5rem' } }}
-
-              />
-
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                  label="birthdate"
-                  value={form.birthdate}
-                  onChange={handleDateChange}
-                  renderInput={(params) => <TextField fullWidth {...params} name="birthdate" />}
-
-                />
-              </LocalizationProvider>
-            </Grid>
           </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              name="lastname"
+              label="Lastname"
+              value={form.lastname}
+              onChange={handleInputChange}
+              style={style.input}
+              required
+              sx={{ ml: { md: '1.5rem' } }}
+              disabled={disabled}
+            />
 
-          <Button size="large" variant="contained" type="submit" style={style.submitBtn}>
-            Save Changes
-          </Button>
-        </Paper>
-      </Grid>
-    </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+
+            <TextField
+              fullWidth
+              name="email"
+              label="Email"
+              value={form.email}
+              onChange={handleInputChange}
+              style={style.input}
+              required
+              sx={{ mr: { md: '1.5rem' } }}
+              disabled={disabled}
+            />
+
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              name="phonenumber"
+              label="Phone Number"
+              value={form.phonenumber}
+              onChange={handleInputChange}
+              style={style.input}
+              sx={{ ml: { md: '1.5rem' } }}
+              disabled={disabled}
+
+            />
+
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="birthdate"
+                value={form.birthdate}
+                onChange={handleDateChange}
+                renderInput={(params) => <TextField fullWidth {...params} name="birthdate" />}
+                disabled={disabled}
+              />
+            </LocalizationProvider>
+          </Grid>
+        </Grid>
+
+        <Button size="large" variant="contained" type="submit" style={style.submitBtn}>
+          Save Changes
+        </Button>
+      </Paper >
+
+    </Section>
   );
 }
 
@@ -151,13 +157,13 @@ const style = {
     fontWeight: '1000' as string,
     color: '#2b3445'
   } as React.CSSProperties,
+  headerIcon: {
+    color: '#d23f57'
+  } as React.CSSProperties,
   form: {
   } as React.CSSProperties,
   paperWrapper: {
     rowGap: '2rem'
-  } as React.CSSProperties,
-  headerIcon: {
-    color: '#d23f57'
   } as React.CSSProperties,
   input: {
 
