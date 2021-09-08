24727ccede1e0926e3f3237f5bb29c4f8c45be04
LahmerMohammed <tonysycho14@gmail.com>
2021-09-08 23:05:06 +0100

    create

---
+++ src/types/UserTypes.ts
@@ -0,0 +1,15 @@
+
+export interface LoginDto {
+  email: string,
+  password: string,
+}
+
+export interface RegisterDto {
+  username: string,
+  email: string,
+  password: string,
+}
+
+export interface ConfirmEmailDto {
+  token: string,
+}
