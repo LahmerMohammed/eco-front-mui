f38417ab597d82735b1b2f6416b565e8ac582562
LahmerMohammed <tonysycho14@gmail.com>
2021-09-13 17:14:41 +0100

    create

---
+++ src/routes/main/mainRoutes.tsx
@@ -0,0 +1,19 @@
+// prettier-ignore
+import * as React from 'react';
+import { Switch } from 'react-router';
+
+interface Props {
+
+}
+
+export function MainRoutes(props: Props) {
+
+
+  return (
+    <Switch>
+
+    </Switch>
+  );
+
+}
+
