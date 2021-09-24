b3a46e29d92eff523859d71a877fc4ba9ce7a1fb
LahmerMohammed <tonysycho14@gmail.com>
2021-09-24 09:54:54 +0100

    refactor

--- src/services/base.ts
+++ src/services/base.ts
@@ -3,4 +3,18 @@ import axios from 'axios'
 
 export  const api = axios.create({
   baseURL: "https://ecommerce-2000414.herokuapp.com/",
-})
+});
+
+
+export const setToken = (token: string) => {
+  
+  api.interceptors.request.use((config) => {
+    
+    config.headers.Authprization = token;
+
+    return config;
+
+  });
+
+}
+
