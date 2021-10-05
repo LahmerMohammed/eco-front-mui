dadcb24e0d4ce99a00b766f6826fe28f2c84fb89
LahmerMohammed <tonysycho14@gmail.com>
2021-10-05 10:45:58 +0100

    create

---
+++ src/icons/NewProductIcon.tsx
@@ -0,0 +1,13 @@
+import { SvgIcon, SvgIconProps } from "@mui/material";
+import React from "react";
+
+
+export function NewProductIcon(props: SvgIconProps) {
+
+  return (
+    <SvgIcon {...props}>
+      <path d="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall nav-icon css-1ctfzzu-MuiSvgIcon-root"></path>
+    </SvgIcon>
+  )
+
+}
