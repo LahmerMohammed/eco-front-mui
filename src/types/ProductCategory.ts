ca75e959d21977fdb3f54e3c9017214b9c9a04c6
LahmerMohammed <tonysycho14@gmail.com>
2021-10-09 22:01:31 +0100

    create

---
+++ src/types/ProductCategory.ts
@@ -0,0 +1,10 @@
+export enum ProductCategory {
+  SPORT='sport',
+  HEALTH='health',
+  HOME='home',
+  CHILDREN='children',
+  OTHER='other',
+  ANIMALS='animals',
+  FOOD='food',
+  TO_BE_CATEGORIZED="to_be_categorized"
+ }
