ad2430db1825c95e50cd334efb712257f25e9c96
LahmerMohammed <lahmermohammed65@gmail.com>
2022-06-19 22:45:05 +0100

    update: [name of props]

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,10 @@
 // prettier-ignore
-import { Card, Chip, Grid, Paper, Theme, Typography , Rating, Link } from '@mui/material';
+import { Card, Chip, Grid, Paper, Theme, Typography, Rating, Link } from '@mui/material';
 import { createStyles, makeStyles } from '@mui/styles';
 import * as React from 'react';
 
 export interface ProductProps {
-  discount: number;
+  discount?: number;
   imgPath: string;
   name: string;
   rating: number;
@@ -14,43 +14,43 @@ export interface ProductProps {
 
 
 
-const getNewPrice = (price: number , discount_perc: number) : number => price*discount_perc/100;
+const getNewPrice = (price: number, discount_perc: number): number => price * discount_perc / 100;
 
 export function ProductCard(props: ProductProps) {
 
-  const {  } = props;
+  const { } = props;
   const classes = useStyles();
 
   return (
     <Paper style={style.paper} elevation={0}>
       <Link href="#">
         <div className="chip">
-          <Chip label={`${props.discount}% off`} style={style.chip}/>
+          <Chip label={`${props.discount}% off`} style={style.chip} />
         </div>
         <div className="img">
-          <img className={classes.img} src={props.imgPath}/>
+          <img className={classes.img} src={props.imgPath} />
         </div>
       </Link>
       <Grid >
-        <Grid item container direction="column" rowGap={.5} style={{padding: '1rem'}}>
+        <Grid item container direction="column" rowGap={.5} style={{ padding: '1rem' }}>
           <Grid item >
             <Typography>{props.name}</Typography>
           </Grid>
           <Grid item >
-            <Rating  value={props.rating} readOnly />
+            <Rating value={props.rating} readOnly />
           </Grid>
-          <Grid item container spacing={2} style={{height: '2rem'}}>
+          <Grid item container spacing={2} style={{ height: '2rem' }}>
             <Grid item>
-              <Typography paragraph fontSize="1.25rem" style={{color: '#d23f57'}}>$187.00</Typography>
+              <Typography paragraph fontSize="1.25rem" style={{ color: '#d23f57' }}>$187.00</Typography>
             </Grid>
             <Grid item>
-              <Typography component="del" fontSize="1.25rem" style={{color:'rgb(125, 135, 156)'}}>$200.00</Typography>
+              <Typography component="del" fontSize="1.25rem" style={{ color: 'rgb(125, 135, 156)' }}>$200.00</Typography>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
     </Paper>
-    
+
   );
 }
 
@@ -58,7 +58,7 @@ const style = {
   chip: {
     marginTop: '.5rem',
     marginLeft: '.5rem',
-    backgroundColor: '#d23f57' ,
+    backgroundColor: '#d23f57',
     color: 'white',
   },
   paper: {
@@ -69,8 +69,8 @@ const style = {
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   imgWrap: {},
-  
-  img:{
+
+  img: {
     width: '-moz-available'
   }
 }));
