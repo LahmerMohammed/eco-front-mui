bf0bb910e78ef816032ba279b94a8725351ca70c
LahmerMohammed <lahmermohammed65@gmail.com>
2022-06-19 22:48:32 +0100

    add: [fetch products data from backend + pagination]

--- src/services/vendor.service.ts
+++ src/services/vendor.service.ts
@@ -5,12 +5,52 @@ import {api} from './base'
 
 
 import { ICreateProductDTO } from '../types/product.create.dto';
+import { AxiosError } from 'axios';
+import { Stream } from 'stream';
+
+
+interface GetProductsOptions{
+  limit?: number,
+  offset?: number,
+  page?: number,
+}
 
 
 
+interface GetProductResponse{
+  data: {image:any,name:string,regular_price:number,sale_price:string,rating:number}[];
+  count: number;
+  total: number;
+  page: number;
+  pageCount: number; 
+}
 
 class VendorService
 {
+  async getProducts(options: GetProductsOptions) {
+    try{
+      const data = <GetProductResponse>(await(await api.get('products', {
+        params: options
+      })).data);
+      
+      console.log(typeof data.data[0].image)
+
+      let products =  data.data;
+
+      products.forEach((product,index) => {
+        const base64String = btoa(String.fromCharCode(...new Uint8Array(product.image.data)));
+        products[index] = {
+          ...product,
+          image: `data:image/jpg;base64,${base64String}`
+        }
+      })
+      
+      return { ...data , data: products};
+
+    }catch(error: any){
+      console.log(error.response.data);
+    }
+  }
 
   
   async addProduct(productDTO: ICreateProductDTO)
